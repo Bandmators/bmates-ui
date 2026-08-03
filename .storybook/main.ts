a710788155ea4ecbeae34d81f9c72ef0aae092d9
kyechan99 <kyechan99@gmail.com>
2026-08-03 22:33:16 +0900

    fix(ci): update Chromatic workflow and resolve Storybook build failure

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -17,5 +17,13 @@ const config = {
   core: {
     builder: '@storybook/builder-vite',
   },
+  viteFinal(viteConfig) {
+    viteConfig.plugins = viteConfig.plugins?.filter(plugin => {
+      if (!plugin || typeof plugin !== 'object' || Array.isArray(plugin)) return true;
+      return !('name' in plugin && plugin.name === 'vite:dts');
+    });
+
+    return viteConfig;
+  },
 } satisfies StorybookConfig;
 export default config;
